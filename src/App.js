c3203570a141ec6943fe95f63be746fbd2894065
Vincent Nguyen <vqn1204@gmail.com>
2018-12-17 23:43:29 -0500

    added about section, testing mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Banner from "./components/banner";
+import About from "./components/about";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Banner />
+        <About />
       </React.Fragment>
     );
   }
